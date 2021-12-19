a9bd63a77e529e9f24ae830aa80c93fc56f501ef
IsraelGomez-12 <74969291+IsraelGomez-12@users.noreply.github.com>
2021-12-19 07:07:57 -0400

    fix: left some files

---
+++ ApiBooks/Services/BookServices.cs
@@ -0,0 +1,106 @@
+﻿using ApiBooks.Interfaces;
+using ApiBooks.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ApiBooks.Services
+{
+    public class BookServices : IBook
+    {
+        private HttpClient client;
+        private List<Book> bookList;
+        private Book book;
+
+        public BookServices(HttpClient _client)
+        {
+            client = _client;
+        }
+
+        /// <summary>
+        /// This to create data in the fakeApi
+        /// </summary>
+        /// <param name="book"></param>
+        /// <returns>Object</returns>
+        public async Task<Book> CreateBook(Book book)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync("https://fakerestapi.azurewebsites.net/api/v1/Books",book);
+
+            response.EnsureSuccessStatusCode();
+            book = await response.Content.ReadAsAsync<Book>();
+
+            return book;
+
+        }
+
+        /// <summary>
+        /// Method to delete data from FakeApi
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>StatusCode</returns>
+        public async Task<HttpStatusCode> DeleteBook(int id)
+        {
+            HttpResponseMessage response = await client.DeleteAsync($"https://fakerestapi.azurewebsites.net/api/v1/Books/{id}");
+
+            return response.StatusCode;
+        }
+
+        /// <summary>
+        /// Method to get all data from Api
+        /// </summary>
+        /// <returns>IEnumerable</returns>
+        public async Task<List<Book>> GetBooks()
+        {
+            
+            HttpResponseMessage response = await client.GetAsync("https://fakerestapi.azurewebsites.net/api/v1/Books");
+             
+            if(response.IsSuccessStatusCode)
+            { 
+                var result = response.Content.ReadAsStringAsync();
+
+                bookList = JsonConvert.DeserializeObject<List<Book>>(result.Result);
+            }
+
+            return bookList;
+        }
+
+        /// <summary>
+        /// Method to get book by id parameter from FakeApi
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Object</returns>
+        public async Task<Book> GetBookById(int id)
+        {
+            HttpResponseMessage response = await client.GetAsync($"https://fakerestapi.azurewebsites.net/api/v1/Books/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var result = response.Content.ReadAsStringAsync();
+
+                book = JsonConvert.DeserializeObject<Book>(result.Result);
+            }
+
+            return book;
+
+        }
+
+        /// <summary>
+        /// Method to updata date from FackeApi
+        /// </summary>
+        /// <param name="book"></param>
+        /// <returns>Object</returns>
+        public async Task<Book> UpdateBook(Book book)
+        {
+            HttpResponseMessage response = await client.PutAsJsonAsync($"https://fakerestapi.azurewebsites.net/api/v1/Books/{book.id}", book);
+
+            response.EnsureSuccessStatusCode();
+            book = await response.Content.ReadAsAsync<Book>();
+
+            return book;
+        }
+    }
+}
---
+++ ApiBooks/Startup.cs
@@ -0,0 +1,98 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiBooks.Interfaces;
+using ApiBooks.Services;
+
+namespace ApiBooks
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddCors();
+
+   
+
+            services.AddControllers()
+                .AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+           
+            services.AddHttpClient<IBook, BookServices>();
+            AddSwagger(services);
+        }
+
+        private void AddSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                var groupName = "v1";
+
+                options.SwaggerDoc(groupName, new OpenApiInfo
+                {
+                    Title = $"Foo {groupName}",
+                    Version = groupName,
+                    Description = "Foo API",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Foo Company",
+                        Email = string.Empty,
+                        Url = new Uri("https://foo.com/"),
+                    }
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true) // allow any origin
+                .AllowCredentials());
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Foo API V1");
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
